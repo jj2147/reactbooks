c36db962dc6db319f93966bc39f32cf13c2ff351
jason jia <jasonpjia@gmail.com>
2019-01-16 00:04:40 -0600

    m

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,10 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Books from "./pages/Books";
 
-import Nav from "./components/Nav";
-
 function App() {
   return (
     <Router>
       <div>
-        <Nav />
         <Switch>
           <Route exact path="/" component={Books} />
 
